084c136ea092dd9f3cdcd32d0a74996b7a664a78
Germán Aliprandi <galiprandi@gmail.com>
2021-02-24 14:56:09 -0300

    minor changes

--- svelte/src/components/App.svelte
+++ svelte/src/components/App.svelte
@@ -44,8 +44,6 @@
     movies = await fetch(url)
       .then((response) => response.json())
       .then((data) => data.results || [])
-
-    console.log(movies)
   }
 
   const inputLoaded = (input) => {
