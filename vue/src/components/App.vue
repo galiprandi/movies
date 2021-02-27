91cecf744d38c4d0846a4a6f11b8cfa67c8d8a8a
Germán Aliprandi <galiprandi@gmail.com>
2021-02-27 20:37:56 -0300

    Vue build

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -1,63 +1,38 @@
 <template>
   <main>
-    <form @submit.prevent="queryMovies(query)">
+    <form @submit.prevent="queryMovies()">
       <input
-        ref="querySearch"
         id="querySearch"
         name="querySearch"
-        placeholder="Search movies..."
-        :value="query"
+        type="search"
+        v-model="query"
+        placeholder="Search movie..."
       />
       <button type="search">Search</button>
     </form>
     <section>
-      <article
-        v-for="{
-          id,
-          title,
-          poster_path: src,
-          overview,
-          release_date: date,
-        } in movies"
-        :key="id"
-      >
-        <picture>
-          <img
-            v-if="src"
-            :src="'https://image.tmdb.org/t/p/original' + src"
-            :alt="title"
-            :title="title"
-          />
-          <img
-            v-else
-            src="https://via.placeholder.com/500x750?text=POSTER%20UNAVAILABLE"
-            :alt="title"
-            :title="title"
-          />
-        </picture>
-        <section class="summary">
-          <h1>{{ title }} {{ new Date(date).getFullYear() }}</h1>
-          <details>
-            <summary>Summary</summary>
-            <p>{{ overview }}</p>
-          </details>
-        </section>
-      </article>
+      <template v-for="movie in movies" :key="movie.id">
+        <movie-card :movie="movie" />
+      </template>
     </section>
     <p>Power by themoviedb.org</p>
   </main>
 </template>
 
 <script>
-export default {
-  name: "App",
-  data: () => ({
-    query: "",
-    movies: [],
-  }),
+import { provide, ref } from "vue";
+import MovieCard from "./MovieCard.vue";
 
-  methods: {
-    queryMovies: async function (querySearch) {
+export default {
+  components: { MovieCard },
+  setup() {
+    const query = ref("");
+    const movies = ref([]);
+    provide("movies", movies);
+
+    const queryMovies = async () => {
+      const querySearch = query.value;
+
       if (querySearch.length < 2) return;
 
       const API_KEY = `38d2b879e4e32254577f8d6411c67af4`;
@@ -73,9 +48,10 @@ export default {
       return await fetch(API_ENDPOINT)
         .then((response) => response.json())
         .then((data) => {
-          this.movies = data.results.sort(orderResultsNyYears) || [];
+          movies.value = data.results.sort(orderResultsNyYears) || [];
         });
-    },
+    };
+    return { query, movies, queryMovies };
   },
 };
 </script>
--- vue/src/components/MovieCard.vue
+++ vue/src/components/MovieCard.vue
@@ -1,8 +1,37 @@
 <template>
   <article>
-    {{ movie }}
+    <picture>
+      <img
+        v-if="movie.poster_path"
+        :src="'https://image.tmdb.org/t/p/original' + movie.poster_path"
+        :alt="movie.title"
+        :title="movie.title"
+      />
+      <img
+        v-else
+        src="https://via.placeholder.com/500x750?text=POSTER%20UNAVAILABLE"
+        :alt="movie.title"
+        :title="movie.title"
+      />
+    </picture>
+    <section class="summary">
+      <h1>
+        {{ movie.title }} ({{ new Date(movie.release_date).getFullYear() }})
+      </h1>
+      <details>
+        <summary>Summary</summary>
+        <p>{{ movie.overview }}</p>
+      </details>
+    </section>
   </article>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    movie: {
+      type: Object,
+      require: true,
+    },
+  },
+};
 </script>
