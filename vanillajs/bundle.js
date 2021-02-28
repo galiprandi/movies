55bdab59a06ce86a0dcd4ed39fedcb6131a72fbd
Germán Aliprandi <galiprandi@gmail.com>
2021-02-28 13:13:31 -0300

    Improve DOM render

--- vanillajs/bundle.js
+++ vanillajs/bundle.js
@@ -22,30 +22,36 @@ async function queryMovies(querySearch) {
 function renderMovies(movies, container = document.getElementById("movies")) {
   if (!container || !movies) return
   container.innerHTML = ""
-  movies.map((movie) => (container.innerHTML += renderCard(movie)))
+  movies.map((movie) => container.appendChild(renderCard(movie)))
 }
 
 function renderCard(movie) {
-  const date = new Date(movie.release_date).getFullYear()
-  return `
-        <article>
-        <picture
-        ><img
-            src="https://image.tmdb.org/t/p/original/${movie.poster_path}"
-            alt="${movie.title}"
-            title="${movie.title}"
-        /></picture>
-        <section class="summary">
-        <h1>${movie.title} (${date})</h1>
-        <details>
-            <summary>Summary</summary>
-            <p>
-            ${movie.overview}
-            </p>
-        </details>
-        </section>
-    </article>
-    `
+  // Create a Fragment
+  const template = document.importNode(
+    document.getElementById("MovieCard").content,
+    true
+  )
+
+  const year = new Date(movie.release_date).getFullYear()
+  const src = movie.poster_path
+    ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+    : "https://via.placeholder.com/500x750?text=POSTER%20UNAVAILABLE"
+
+  // Set image
+  const img = template.querySelector("img")
+  img.setAttribute("src", src)
+  img.setAttribute("alt", movie.title)
+  img.setAttribute("title", movie.title)
+
+  // Set title
+  const title = template.querySelector(".summary h1")
+  title.textContent = `${movie.title} (${year})`
+
+  // Set overview
+  const overview = template.querySelector(".summary p")
+  overview.textContent = movie.overview
+
+  return template
 }
 
 function handleSubmit(event) {
