28df104f257b04f949c9bf598a7157b74ca42d21
Germán Aliprandi <galiprandi@gmail.com>
2021-02-28 15:20:23 -0300

    More improvements

--- vanillajs/bundle.js
+++ vanillajs/bundle.js
@@ -1,5 +1,14 @@
+async function handleSubmit(event) {
+  event.preventDefault()
+  const query = document.getElementById("querySearch").value
+  const movies = await queryMovies(query)
+  renderMovies(movies)
+}
+
+// Fetch data from API
 async function queryMovies(querySearch) {
-  if (querySearch.length < 2) return
+  querySearch = querySearch.trim()
+  if (querySearch.length <= 2) return
 
   const API_KEY = `38d2b879e4e32254577f8d6411c67af4`
   const API_ENDPOINT = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${querySearch}`
@@ -13,51 +22,47 @@ async function queryMovies(querySearch) {
 
   return await fetch(API_ENDPOINT)
     .then((response) => response.json())
-    .then((data) => {
-      movies = data.results.sort(orderResultsNyYears) || []
-      renderMovies(movies, document.getElementById("movies"))
-    })
+    .then((data) => data.results.sort(orderResultsNyYears) || [])
 }
 
+// Render movies cards to DOM
 function renderMovies(movies, container = document.getElementById("movies")) {
-  if (!container || !movies) return
   const movieList = document.createDocumentFragment()
-  movies.map((movie) => movieList.appendChild(renderCard(movie)))
+  movies.map((movie) => movieList.appendChild(renderMovieCard(movie)))
   container.textContent = "" // Empty movie list
   container.appendChild(movieList) // Append movie list to DOM
 }
 
-function renderCard(movie) {
-  // Create a Fragment
-  const template = document.importNode(
-    document.getElementById("MovieCard").content,
-    true
-  )
+// Create and return a template node with movie card.
+function renderMovieCard(movie) {
+  const { title, release_date: date, poster_path: poster, overview } = movie
 
-  const year = new Date(movie.release_date).getFullYear()
-  const src = movie.poster_path
-    ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+  // Create a card like a DOM fragment
+  const card = document.createDocumentFragment()
+
+  // Append template
+  const template = document.getElementById("MovieCard").content
+  card.appendChild(template.cloneNode(true))
+
+  // Set new data to Fragment
+  const year = new Date(date).getFullYear()
+  const src = poster
+    ? `https://image.tmdb.org/t/p/original/${poster}`
     : "https://via.placeholder.com/500x750?text=POSTER%20UNAVAILABLE"
 
-  // Set image
-  const img = template.querySelector("img")
-  img.setAttribute("src", src)
-  img.setAttribute("alt", movie.title)
-  img.setAttribute("title", movie.title)
+  // Set image poster
+  const movieImage = card.querySelector("img")
+  movieImage.setAttribute("src", src)
+  movieImage.setAttribute("alt", title)
+  movieImage.setAttribute("title", title)
 
-  // Set title
-  const title = template.querySelector(".summary h1")
-  title.textContent = `${movie.title} (${year})`
+  // Set title and year
+  const movieTitle = card.querySelector(".summary > h1")
+  movieTitle.textContent = `${title} (${year})`
 
   // Set overview
-  const overview = template.querySelector(".summary p")
-  overview.textContent = movie.overview
+  const movieOverview = card.querySelector(".summary p")
+  movieOverview.textContent = `${overview}`
 
-  return template
-}
-
-function handleSubmit(event) {
-  event.preventDefault()
-  const query = document.forms[0][0].value || ""
-  queryMovies(query)
+  return card
 }
